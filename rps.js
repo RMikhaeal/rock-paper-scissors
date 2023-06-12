93cdc05083a63f0943fe75af94e01e6e2c1b7830
AcousticSavant <123994190+RMikhaeal@users.noreply.github.com>
2023-06-12 14:00:20 -0400

    Add button functionality

--- rps.js
+++ rps.js
@@ -1,81 +1,82 @@
-function game() {
-    let winner, gameWinner;
-    let player = 0, comp = 0;
-    
-    for (let i = 0; i < 5; i++) {
-        winner = (playRound(getPlayerChoice(), getComputerChoice()));
-        console.log(winner);
+let winner;
+let player = 0, comp = 0;
 
-        if (winner.includes("win")) {
-            player += 1;
-        } else if (winner.includes("lose")) {
-            comp += 1;
-        }
-    }
+const buttons = document.querySelectorAll('button');
 
-    if (player > comp) {
-        gameWinner = "Player wins!";
-    } else if (comp > player) {
-        gameWinner = "Computer wins!";
-    } else {
-        gameWinner = "Tie game!";
-    }
-
-    console.log(player);
-    console.log(comp);
-    return gameWinner;
-}
+buttons.forEach((button) => {
+	button.addEventListener('click', () => {
+		winner = playRound(button.id, getComputerChoice());
+	});
+});
 
 function playRound(playerSelection, computerSelection) {
-    const playerChoice = playerSelection.toLowerCase();
-    let result;
+	const playerChoice = playerSelection.toLowerCase();
+	let result;
 
-    if (playerChoice == computerSelection) {
-        result = "Tie! Both used " + playerChoice;
-        return result;
-    }
+	if (playerChoice == computerSelection) {
+		result = "Tie! Both used " + playerChoice + ".";
+		document.getElementById('result').innerHTML = result + " Pick again!";
+		return result;
+	}
 
-    switch(computerSelection) {
-        case "rock":
-            (playerChoice == "paper") ? result = "You win! Paper beats rock" : result = "You lose! Rock beats scissors";
-            break;
-        case "paper":
-            (playerChoice == "scissors") ? result = "You win! Scissors beats paper" : result = "You lose! Paper beats rock";
-            break;
-        case "scissors":
-            (playerChoice == "rock") ? result = "You win! Rock beats scissors" : result = "You lose! Scissors beats paper";
-    }
+	switch(computerSelection) {
+		case "rock":
+			(playerChoice == "paper") ? result = "You win! Paper beats rock." : result = "You lose! Rock beats scissors.";
+			break;
+		case "paper":
+			(playerChoice == "scissors") ? result = "You win! Scissors beats paper." : result = "You lose! Paper beats rock.";
+			break;
+		case "scissors":
+			(playerChoice == "rock") ? result = "You win! Rock beats scissors." : result = "You lose! Scissors beats paper.";
+	}
 
-    return result;
+	if (result.includes("win")) {
+		player += 1;
+	} else if (result.includes("lose")) {
+		comp += 1;
+	}
+
+	document.getElementById('result').innerHTML = result + " Pick again!";
+	document.getElementById('playerscore').innerHTML = "Player score: " + player;
+	document.getElementById('computerscore').innerHTML = "Computer score: " + comp;
+
+	if (player == 5) {
+		setTimeout(function() {
+			alert("Player wins!");
+			resetScores();
+		},10)
+	} else if (comp == 5) {
+		setTimeout(function() {
+			alert("Computer wins!");
+			resetScores();
+		},10)
+	}
+
+	return result;
 }
 
 function getComputerChoice() {
-    let cpu = Math.floor(Math.random() * 3);
-    let choice;
-    
-    switch(cpu) {
-        case 0:
-            choice = "rock";
-            break;
-        case 1:
-            choice = "paper";
-            break;
-        case 2:
-            choice = "scissors"
-    }
+	let cpu = Math.floor(Math.random() * 3);
+	let choice;
+	
+	switch(cpu) {
+		case 0:
+			choice = "rock";
+			break;
+		case 1:
+			choice = "paper";
+			break;
+		case 2:
+			choice = "scissors"
+	}
 
-    return choice;
+	return choice;
 }
 
-function getPlayerChoice() {
-    let pchoice;
-
-    do {
-        pchoice = prompt("Rock, paper, or scissors?").toLowerCase();
-    }
-    while (pchoice != "rock" && pchoice != "paper" && pchoice != "scissors");
-
-    return pchoice;
+function resetScores() {
+	player = 0; 
+	comp = 0;
+	document.getElementById('result').innerHTML = "Rock, Paper, or Scissors?";
+	document.getElementById('playerscore').innerHTML = "Player score: " + player;
+	document.getElementById('computerscore').innerHTML = "Computer score: " + comp;
 }
-
-console.log(game());
